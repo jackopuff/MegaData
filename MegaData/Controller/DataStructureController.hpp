b2bfaf2a8095d04da107fc7eda96bffeb597a81b
jackopuff <jackson@carterrealm.com>
2017-02-21 12:01:28 -0700

    added MegaData to computer

---
+++ MegaData/Controller/DataStructureController.hpp
@@ -0,0 +1,25 @@
+//
+//  DataStructureController.hpp
+//  MegaData
+//
+//  Created by Carter, Jackson on 2/8/17.
+//  Copyright © 2017 Carter, Jackson. All rights reserved.
+//
+
+#ifndef DataStructureController_hpp
+#define DataStructureController_hpp
+
+#include "../Nodel/Node.hpp"
+#include <string>
+
+class DataStructureController
+{
+private:
+    void testIntArray();
+public:
+    DataStructureController();
+    void start();
+};
+
+
+#endif /* DataStructureController_hpp */
